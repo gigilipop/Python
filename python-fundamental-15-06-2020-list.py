d8d5bd181cea87e7648172d5c3d64e3587022915
priyanka <48902102+priyankaVi@users.noreply.github.com>
2020-07-29 21:37:31 +0530

    Add files via upload

---
+++ python-fundamental-15-06-2020-list.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+##list
+
+
+# In[ ]:
+
+
+##list is defined in in square bracket []
+
+
+# In[2]:
+
+
+students= []
+
+
+# In[11]:
+
+
+students = ["priyanka","devi"]
+print(students)
+
+
+# In[14]:
+
+
+apple=["f","g","a","b"]
+print(apple)
+
+
+# In[13]:
+
+
+type(apple)
+
+
+# In[15]:
+
+
+print(apple)
+
+
+# In[16]:
+
+
+print(apple[3])
+
+
+# In[ ]:
+
+
+##to find the element based on to the positioning
+
+
+# In[17]:
+
+
+print(apple)
+
+
+# In[18]:
+
+
+print(apple[2])##it prints based on the position
+
+
+# In[19]:
+
+
+##to enhance the list elements i a defined manner
+
+
+# In[20]:
+
+
+print(apple
+    )
+
+
+# In[21]:
+
+
+print(f is alphabet,{apple})
+
+
+# In[22]:
+
+
+print("f is the alphabet",{apple})
+
+
+# In[23]:
+
+
+print("f is the alphabet,{apple[2]}")
+
+
+# In[24]:
+
+
+print(apple)
+
+
+# In[25]:
+
+
+print(f"it is an alphabet,{apple[1]}")
+
+
+# In[26]:
+
+
+print(apple)
+
+
+# In[27]:
+
+
+##add another alhabet
+
+
+# In[28]:
+
+
+apple.append('zz')##append it will add in last place
+print(apple)
+
+
+# In[30]:
+
+
+#to add in desired place
+apple.insert(3,'abcd')###add only text in colon
+
+
+# In[31]:
+
+
+print(apple)
+
+
+# In[32]:
+
+
+apple[2]="ghj"
+print(apple)
+
+
+# In[33]:
+
+
+del apple(2)
+print(apple)
+
+
+# In[34]:
+
+
+del apple[2]
+print(apple)
+
+
+# In[ ]:
+
+
+
+

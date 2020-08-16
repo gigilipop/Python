8b006b392bc870fb5342b8b654809b5a36ecbc7f
priyanka <48902102+priyankaVi@users.noreply.github.com>
2020-08-16 20:18:23 +0530

    Add files via upload

---
+++ challenge 3 #strings.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+###challenge3 STRINGSabs
+
+
+# In[ ]:
+
+
+020 
+## Ask the user to enter their first name 
+    ##and then display the length of their name
+    
+
+
+# In[2]:
+
+
+user=input("nter the first name")
+len(user)
+
+
+# In[ ]:
+
+
+021 
+#Ask the user to enter their first name 
+#and then ask them to enter their surname.
+#Join them together with a space between and 
+display the name 
+#and the length of whole name.
+
+
+# In[4]:
+
+
+user=input("enter the first name")
+surname=input("enter the surname")
+name= user+" "+surname
+print("name")
+len(name)
+length=len(name)
+print(length)
+
+
+# In[ ]:
+
+
+###Ask the user to enter their first name 
+and surname in lower case.
+Change the case to title case
+and join them together. 
+Display the finished result. 
+023 
+
+
+# In[7]:
+
+
+user=input("enter the fiorstname")
+surname=input("enter the surname")
+surname.lower()
+print(loweer)
+
+
+# In[ ]:
+
+
+#Ask the user to enter their first name.
+#If the length of their first name is under five characters,
+#ask them to enter their surname and 
+join them together (without a space) and 
+display the name in upper case.
+If the length of the first name is five or more characters, 
+display their first name in lower case. 
+026 
+
+
+# In[12]:
+
+
+user=input("enter the firstname")
+
+if len(name)<5:
+    sur=input("enter the surname")
+    user=user+sur
+else:
+    print(user.lower())
+    
+
+    
+    
+
+
+# In[11]:
+
+
+user=input("enter the first name")
+user=user.upper()
+print(user)
+
+
+# In[ ]:
+
+
+026 
+ ###Pig Latin takes the first consonant of a word,
+    moves it to the end of the word 
+    and adds on an “ay”. 
+    If a word begins with a vowel you just add “way”
+    to the end. For example, pig becomes igpay, 
+    banana becomes ananabay, and
+    aadvark becomes aadvarkway. 
+    Create a program that will ask the
+    user to enter a word and change it into Pig Latin. 
+    Make sure the new word is displayed in lower case. 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
